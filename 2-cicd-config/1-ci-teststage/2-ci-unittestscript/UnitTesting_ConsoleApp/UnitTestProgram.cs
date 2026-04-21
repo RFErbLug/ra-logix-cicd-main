0c719c4e4fab1b7fadc807cacb519ea02d8cb58c
RFErbLug <rferblug@ra.rockwell.com>
2026-04-21 16:33:15 -0400

    adding login

--- 2-cicd-config/1-ci-teststage/2-ci-unittestscript/UnitTesting_ConsoleApp/UnitTestProgram.cs
+++ 2-cicd-config/1-ci-teststage/2-ci-unittestscript/UnitTesting_ConsoleApp/UnitTestProgram.cs
@@ -5,6 +5,9 @@ Console.WriteLine("=== ACD → CONTROLLER → DOWNLOAD TEST START ===");
 
 var serviceClient = ClientFactory.GetServiceApiClientV2("CI_Demo", 46520);
 
+// ADD THIS LINE ONLY
+serviceClient.LoginFactoryTalkUser("desktop-c2jqv6k\\jenkins", "Rockwell1");
+
 string acdPath = @"C:\CI-Pipeline-Files\test.ACD";
 
 // 1. Get chassis
