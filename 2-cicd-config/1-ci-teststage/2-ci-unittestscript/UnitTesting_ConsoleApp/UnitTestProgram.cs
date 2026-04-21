e4d049541cb2a5e4679096f50cd99ae8607bede8
RFErbLug <rferblug@ra.rockwell.com>
2026-04-21 18:20:51 -0400

    test download no login

--- 2-cicd-config/1-ci-teststage/2-ci-unittestscript/UnitTesting_ConsoleApp/UnitTestProgram.cs
+++ 2-cicd-config/1-ci-teststage/2-ci-unittestscript/UnitTesting_ConsoleApp/UnitTestProgram.cs
@@ -1,37 +1,66 @@
 ﻿using RockwellAutomation.FactoryTalkLogixEcho.Api.Client;
+using RockwellAutomation.FactoryTalkLogixEcho.Api.Interfaces;
 using System;
-using System.Linq;
+using System.Threading.Tasks;
 
-var serviceClient = ClientFactory.GetServiceApiClientV2("ListControllers", 46520);
+Console.WriteLine("=== SAMPLE-STYLE DOWNLOAD TEST START ===");
+
+// CHANGE THESE TWO VALUES
+string acdFilePath = @"C:\CI-Pipeline-Files\test.ACD";
+Guid controllerGuid = Guid.Parse("c8503cca-41e7-4a04-9352-5541488a7840");
+
+// Use your Echo port explicitly
+var serviceClient = ClientFactory.GetServiceApiClientV2("CI_Demo", 46520);
 
 try
 {
-    var chassisList = await serviceClient.ListChassis();
+    Console.WriteLine($"ACD Path: {acdFilePath}");
+    Console.WriteLine($"Controller GUID: {controllerGuid}");
 
-    foreach (var chassis in chassisList)
+    // Confirm controller exists
+    var controller = await serviceClient.ReadController(controllerGuid);
+    Console.WriteLine($"Controller found: {controller.ControllerName}");
+    Console.WriteLine($"Controller ID: {controller.ControllerGuid}");
+
+    // Send file and start download
+    using (var fileHandle = await serviceClient.SendFile(acdFilePath))
     {
-        Console.WriteLine($"Chassis: {chassis.Name}");
-        Console.WriteLine($"ChassisGuid: {chassis.ChassisGuid}");
+        Console.WriteLine("File sent to service.");
+        Console.WriteLine("Starting download...");
+        await serviceClient.Download(controller.ControllerGuid, fileHandle);
+    }
 
-        var controllers = await serviceClient.ListControllers(chassis.ChassisGuid);
+    // Poll feedback
+    DownloadFeedback downloadFeedback;
+    do
+    {
+        downloadFeedback = await serviceClient.GetDownloadFeedback(controller.ControllerGuid);
 
-        if (!controllers.Any())
+        foreach (var message in downloadFeedback.Messages)
         {
-            Console.WriteLine("  No controllers found.");
-        }
-        else
-        {
-            foreach (var controller in controllers)
-            {
-                Console.WriteLine($"  ControllerName: {controller.ControllerName}");
-                Console.WriteLine($"  ControllerGuid: {controller.ControllerGuid}");
-            }
+            Console.WriteLine(message);
         }
 
-        Console.WriteLine();
+        await Task.Delay(500);
+    }
+    while (downloadFeedback.State == OperationState.InProgress);
+
+    Console.WriteLine($"Final state: {downloadFeedback.State}");
+
+    if (downloadFeedback.State == OperationState.Done)
+    {
+        Console.WriteLine("=== DOWNLOAD SUCCEEDED ===");
+        return 0;
+    }
+    else
+    {
+        Console.WriteLine("=== DOWNLOAD FAILED ===");
+        return 1;
     }
 }
 catch (Exception ex)
 {
+    Console.WriteLine("=== ERROR ===");
     Console.WriteLine(ex.ToString());
+    return 1;
 }
